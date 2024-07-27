3c3ed2b0e1b38cf65b62e4c0f29770ec87c03a3f
Admire <admire1971@outlook.com>
2024-07-27 16:03:44 +0800

    1

--- service/src/main/java/io/github/a13e300/tricky_store/keystore/CertHack.java
+++ service/src/main/java/io/github/a13e300/tricky_store/keystore/CertHack.java
@@ -316,7 +316,7 @@ public final class CertHack {
 
             if (element instanceof ASN1TaggedObject) {
                 ASN1TaggedObject taggedObject = (ASN1TaggedObject) element;
-                ASN1Encodable obj = taggedObject.getObject(); // 尝试使用 getObject 方法
+                ASN1Primitive obj = taggedObject.getBaseUniversal(true, -1); // 使用 getBaseUniversal 方法
 
                 if (obj instanceof ASN1ObjectIdentifier) {
                     ASN1ObjectIdentifier oid = (ASN1ObjectIdentifier) obj;
